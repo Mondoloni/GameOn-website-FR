61cb9c3de3e1d8b6028d46e34ccffddff7a20e68
Mondoloni <113922209+Mondoloni@users.noreply.github.com>
2023-08-02 22:00:26 +0200

    TODO : fermer la modale
    Ajouter la fonctionnalité au bouton (x)
    Utilisation de: "display = none" pour fermer la modale.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,14 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+
+//TODO : fermer la modale
+//Pour tous les éléments modalClose on ajoute un événement au click
+modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+// launch modal close
+function closeModal() {
+  //On affecte la propriété display = non ) l'élément modalbg
+  //On cache la modal
+  modalbg.style.display = "none";
+}
 
