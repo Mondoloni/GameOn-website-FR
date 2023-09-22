aea9d819e3a1b3c8a96e5f260e5448aa5e237ed2
Mondoloni <113922209+Mondoloni@users.noreply.github.com>
2023-09-22 16:02:40 +0200

    Finalisation apres soutenantce
    Modification expr reguliere validation champ nombre

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,75 +1,73 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line no-unused-vars
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  const x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
   } else {
-    x.className = "topnav";
+    x.className = 'topnav';
   }
 }
 
-
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-//Elements de fermeture de la modal
-const fermetureModal = document.querySelectorAll(".close");
-//Elements formulaire
-const form=document.querySelector('form');
-//Bouton Fermer sur la modal de confirmation d'inscription
-const btnFermerConfirmation=document.getElementById("btn-fermer");
-
+const modalbg = document.querySelector('.bground');
+const modalBtn = document.querySelectorAll('.modal-btn');
+// eslint-disable-next-line no-unused-vars
+const formData = document.querySelectorAll('.formData');
+// Elements de fermeture de la modal
+const fermetureModal = document.querySelectorAll('.close');
+// Elements formulaire
+const form = document.querySelector('form');
+// Bouton Fermer sur la modal de confirmation d'inscription
+const btnFermerConfirmation = document.getElementById('btn-fermer');
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
+// eslint-disable-next-line no-use-before-define
+modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 // launch modal form
 function launchModal() {
-  //Masque la div de confirmation d'inscription
-  document.querySelector(".formConfirmation").style.display = "none";
-  //Affiche la div d'inscription
-  document.querySelector(".modal-body").style.display = "block";
-  modalbg.style.display = "block";
+  // Masque la div de confirmation d'inscription
+  document.querySelector('.formConfirmation').style.display = 'none';
+  // Affiche la div d'inscription
+  document.querySelector('.modal-body').style.display = 'block';
+  modalbg.style.display = 'block';
 }
 
-//TODO : fermer la modale
-//Pour tous les éléments fermtureModal on ajoute un événement au click
-fermetureModal.forEach((btn) => btn.addEventListener("click", closeModal));
+// TODO : fermer la modale
+// Pour tous les éléments fermtureModal on ajoute un événement au click
+fermetureModal.forEach((btn) => btn.addEventListener('click', closeModal));
 
-//Fonction de fermture de la Modal
+// Fonction de fermture de la Modal
 function closeModal() {
-  //On affecte la propriété display = none à l'élément modalbg
-  //On cache la modal
-  modalbg.style.display = "none";
+  // On affecte la propriété display = none à l'élément modalbg
+  // On cache la modal
+  modalbg.style.display = 'none';
 }
 
-//Ajout d'un évévement lorsque l'on submit le formulaire
-form.addEventListener("submit",(event)=>
-{
-  //On empêche le comportement par défaut
+// Ajout d'un évévement lorsque l'on submit le formulaire
+form.addEventListener('submit', (event) => {
+  // On empêche le comportement par défaut
   event.preventDefault();
-  
-  //Test si tous les champs sont valides à l'aide de fonction
-  if(validerChampCivilite(`first`,"Prénom") && 
-    validerChampCivilite(`last`,"Nom") && 
-    validerEmail(`email`) && 
-    birthdate(`birthdate`) && 
-    validerNbr('quantity') && 
-    validerRadio(`location`,`location6`) &&
-    validerCheckBox(`checkbox1`))
-    {
-      //Si vrai on masque la modal d'inscription
-      document.querySelector(".modal-body").style.display = "none";
-      //Affiche la div de confirmation d'inscription
-      document.querySelector(".formConfirmation").style.display = "block";
-    }
+
+  // Test si tous les champs sont valides à l'aide de fonction
+  if (validerChampCivilite('first', 'Prénom')
+    && validerChampCivilite('last', 'Nom')
+    && validerEmail('email')
+    && birthdate('birthdate')
+    && validerNbr('quantity')
+    && validerRadio('location', 'location6')
+    && validerCheckBox('checkbox1')) {
+    // Si vrai on masque la modal d'inscription
+    document.querySelector('.modal-body').style.display = 'none';
+    // Affiche la div de confirmation d'inscription
+    document.querySelector('.formConfirmation').style.display = 'block';
+  }
 });
 
-//Ajout d'un événement losque l'on clique sur le bouton fermer
-//de la div de confirmation d'inscription
-btnFermerConfirmation.addEventListener("click",(event)=>{
-  //On empêche le comportement par défaut
+// Ajout d'un événement losque l'on clique sur le bouton fermer
+// de la div de confirmation d'inscription
+btnFermerConfirmation.addEventListener('click', (event) => {
+  // On empêche le comportement par défaut
   event.preventDefault();
-  //Masque la modal
-  modalbg.style.display = "none";
-  
-})
+  // Masque la modal
+  modalbg.style.display = 'none';
+});
--- starterOnly/script.js
+++ starterOnly/script.js
@@ -1,205 +1,182 @@
-/** 
+/* eslint-disable linebreak-style */
+/* eslint-disable no-plusplus */
+/* eslint-disable eqeqeq */
+/* eslint-disable consistent-return */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+/**
 **Cette fonction prend deux paramètres : l'id de la balise et la value du label associé
 cette fonction valide que le champ est au bon format
 * ici : deux caractères au minimum
 *Retourne vrai si le champ est valide
 */
-function validerChampCivilite(balise,texte) {
-//Récupération de la balise
-const baliseAValider=document.getElementById(`${balise}`);
-//Appel de la fonction viderBaliseErreur afin de supprimer le message d'erreur 
-//d'une precedente valiaton du formulaire
-viderBaliseErreur(balise)
-    try{
-        if (baliseAValider.value.length < 2) 
-        {
-            throw new Error(`Veuillez entrer 2 caractères ou plus pour le champ du ${texte}. `)
-        }
-        return true
+function validerChampCivilite(balise, texte) {
+// Récupération de la balise
+  const baliseAValider = document.getElementById(`${balise}`);
+  // Appel de la fonction viderBaliseErreur afin de supprimer le message d'erreur
+  // d'une precedente valiaton du formulaire
+  viderBaliseErreur(balise);
+  try {
+    if (baliseAValider.value.length < 2) {
+      throw new Error(`Veuillez entrer 2 caractères ou plus pour le champ du ${texte}. `);
     }
-    catch(erreur){
-        afficherMessageErreur(erreur.message,balise)
-    }
-   
-   
+    return true;
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message, balise);
+  }
 }
 
-/** 
+/**
 **Cette fonction prend un paramètre : l'id de la balise email
-cette fonction valide qu'un champ email est au bon format 
+cette fonction valide qu'un champ email est au bon format
 * ici : XX@XX.XX
 *Retourne vrai si le champ est valide
 */
 function validerEmail(email) {
-    //Récupération de la balise
-    const baliseEmail=document.getElementById(`${email}`)
-    //Création d'une expression régulières de type XX@XX.XX
-    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
-    //Appel de la fonction viderBaliseErreur afin de supprimer le message d'erreur 
-    //d'une precedente valiaton du formulaire
-    viderBaliseErreur(email)
-    try
-    {
-        if (!emailRegExp.test(baliseEmail.value)) 
-        {
-            throw new Error("L'email n'est pas valide.")
-        }
-        return true 
+  // Récupération de la balise
+  const baliseEmail = document.getElementById(`${email}`);
+  // Création d'une expression régulières de type XX@XX.XX
+  const emailRegExp = new RegExp('[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+');
+  // Appel de la fonction viderBaliseErreur afin de supprimer le message d'erreur
+  // d'une precedente valiaton du formulaire
+  viderBaliseErreur(email);
+  try {
+    if (!emailRegExp.test(baliseEmail.value)) {
+      throw new Error("L'email n'est pas valide.");
     }
-    catch(erreur)
-    {
-        afficherMessageErreur(erreur.message,email)
-    } 
+    return true;
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message, email);
+  }
 }
 
-
-/** 
+/**
 **Cette fonction prend un paramètre : l'id de la balise date de naissance
-cette fonction valide qu'un champ date de naissance est au bon format 
+cette fonction valide qu'un champ date de naissance est au bon format
 * ici : 00/00/0000
 *Retourne vrai si le champ est valide
 */
-function birthdate(balise)
-{
-    const baliseBirthdate=document.getElementById(`${balise}`)
-    //Création d'une expression régulières de type 1111-11-11
-    let birthdateRegExp = RegExp(/^\d{4}-\d{2}-\d{2}$/)
-    viderBaliseErreur(balise)
-    try
-    {
-        if (!birthdateRegExp.test(baliseBirthdate.value)) 
-        {
-            throw new Error("Vous devez entrer votre date de naissance.")
-        }
-        return true
+function birthdate(balise) {
+  const baliseBirthdate = document.getElementById(`${balise}`);
+  // Création d'une expression régulières de type 1111-11-11
+  const birthdateRegExp = RegExp(/^\d{4}-\d{2}-\d{2}$/);
+  viderBaliseErreur(balise);
+  try {
+    if (!birthdateRegExp.test(baliseBirthdate.value)) {
+      throw new Error('Vous devez entrer votre date de naissance.');
     }
-    catch(erreur)
-    {
-        afficherMessageErreur(erreur.message,balise)
-    }   
+    return true;
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message, balise);
+  }
 }
 
-/** 
+/**
 **Cette fonction prend un paramètre : l'id de la balise d'un champ nombre
-cette fonction valide que le champ est bien remplie avec des nombres 
+cette fonction valide que le champ est bien remplie avec des nombres
 *Retourne vrai si le champ est valide
-* 
+*
 */
-function validerNbr(balise)
-{
-    const baliseNbr=document.getElementById(`${balise}`)
-     //Création d'une expression régulières de type valeur numérique
-    let nbrRegExp = new RegExp("[0-9]+")
-    viderBaliseErreur(balise)
-    try
-    {
-        if (!nbrRegExp.test(baliseNbr.value)) 
-        {
-            throw new Error("Veuillez saisir un nombre.")
-        }
-        return true
+function validerNbr(balise) {
+  const baliseNbr = document.getElementById(`${balise}`);
+  // Création d'une expression régulières de type valeur numérique
+  const nbrRegExp = new RegExp('^[0-9]\\d*$');
+  viderBaliseErreur(balise);
+  try {
+    if (!nbrRegExp.test(baliseNbr.value)) {
+      throw new Error('Veuillez saisir un nombre.');
     }
-    catch(erreur)
-    {
-        afficherMessageErreur(erreur.message,balise)
-    }
+    return true;
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message, balise);
+  }
 }
 
-/** 
+/**
 **Cette fonction prend deux paramètres : le nom de la balise checkbox et la value du dernier label
 cette fonction valide qu'une checkbox a été sélectionné
 * *Retourne vrai si un boton est sélectionné
 */
-function validerRadio(balise,nomBalise)
-{
-    //Récupération des input de la balise
-    let baliseCheckBox = document.querySelectorAll(`input[name="${balise}"]`)
-    let check = ""
-    viderBaliseErreur(nomBalise)
-    //Pour chaque bouton récupéré on vérifie s'il a été coché
-    //Si c'est le cas nous intérompons la boucle
-    for (let i = 0; i < baliseCheckBox.length; i++) {
-        if (baliseCheckBox[i].checked) {
-            check = baliseCheckBox[i].value
-            break
-        }
+function validerRadio(balise, nomBalise) {
+  // Récupération des input de la balise
+  const baliseCheckBox = document.querySelectorAll(`input[name="${balise}"]`);
+  let check = '';
+  viderBaliseErreur(nomBalise);
+  // Pour chaque bouton récupéré on vérifie s'il a été coché
+  // Si c'est le cas nous intérompons la boucle
+  for (let i = 0; i < baliseCheckBox.length; i++) {
+    if (baliseCheckBox[i].checked) {
+      check = baliseCheckBox[i].value;
+      break;
     }
-    //Si aucun bouton radio de sélectionnée alors on affiche un message d'erreur
-    try{
-        if(check=="")
-        {
-            throw new Error("Vous devez choisir une option.")  
-        }
-        else{
-            afficherMessageErreur("",nomBalise)
-            return true
-        }
+  }
+  // Si aucun bouton radio de sélectionnée alors on affiche un message d'erreur
+  try {
+    if (check == '') {
+      throw new Error('Vous devez choisir une option.');
+    } else {
+      afficherMessageErreur('', nomBalise);
+      return true;
     }
-    catch(erreur){
-        afficherMessageErreur(erreur.message,nomBalise)
-    }
-   
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message, nomBalise);
+  }
 }
 
-/** 
+/**
 **Cette fonction prend un paramètre : l'id de la balise d'un champ checkbox
-cette fonction valide que la checkbox est coché 
+cette fonction valide que la checkbox est coché
 *Retourne vrai si la chexkbox est coché
-* 
+*
 */
-function validerCheckBox(balise)
-{
-    //Récupération de la propriété checked de la balise passée en paramétre
-  let baliseCheckBox = document.getElementById(`${balise}`).checked
-  viderBaliseErreur(balise)
-  try{
-    if(!baliseCheckBox){
-        throw new Error("Vous devez vérifier que vous acceptez les termes et conditions.")  
+function validerCheckBox(balise) {
+  // Récupération de la propriété checked de la balise passée en paramétre
+  const baliseCheckBox = document.getElementById(`${balise}`).checked;
+  viderBaliseErreur(balise);
+  try {
+    if (!baliseCheckBox) {
+      throw new Error('Vous devez vérifier que vous acceptez les termes et conditions.');
     }
-    return true
-  }
-  catch(erreur){
-    afficherMessageErreur(erreur.message,balise)
+    return true;
+  } catch (erreur) {
+    afficherMessageErreur(erreur.message, balise);
   }
 }
 
-/** 
+/**
 **Cette fonction prend deux paramètres : le message d'erreur a affiché
 *et le champ qui n'est pas valide
 */
-function afficherMessageErreur(message,balise) {
-    
-    //Récupére la balise d'un message d'erreur précedement créé
-    let spanErreurMessage = document.getElementById(`erreurMessage${balise}`)
+function afficherMessageErreur(message, balise) {
+  // Récupére la balise d'un message d'erreur précedement créé
+  let spanErreurMessage = document.getElementById(`erreurMessage${balise}`);
 
-    //Si la balise n'existe pas on la créé
-    if (!spanErreurMessage) {
-        //Création d'une balise span
-        spanErreurMessage = document.createElement("span")
-        //Ajout de l'id permettant l'identification
-        spanErreurMessage.id = `erreurMessage${balise}`
-        //Recupération de l'ID du champ non valide passé en paramètre
-        const baliseAValider=document.getElementById(`${balise}`)
-        //Récupération de la balise parent
-        const baliseParent=baliseAValider.parentNode
-        //Ajout de la balise span au parent
-        baliseParent.appendChild(spanErreurMessage)
-        //Ajout du message d'erreur à la balise span
-        spanErreurMessage.innerText = message
-    }
-    
+  // Si la balise n'existe pas on la créé
+  if (!spanErreurMessage) {
+    // Création d'une balise span
+    spanErreurMessage = document.createElement('span');
+    // Ajout de l'id permettant l'identification
+    spanErreurMessage.id = `erreurMessage${balise}`;
+    // Recupération de l'ID du champ non valide passé en paramètre
+    const baliseAValider = document.getElementById(`${balise}`);
+    // Récupération de la balise parent
+    const baliseParent = baliseAValider.parentNode;
+    // Ajout de la balise span au parent
+    baliseParent.appendChild(spanErreurMessage);
+    // Ajout du message d'erreur à la balise span
+    spanErreurMessage.innerText = message;
+  }
 }
 
-/** 
-**Cette fonction prend un paramètre : l'id de la balise d'un champ 
+/**
+**Cette fonction prend un paramètre : l'id de la balise d'un champ
 *cette fonction supprime la balise span avec le message d'erreur lié à la balise passé en paramètre
-* 
+*
 */
-function viderBaliseErreur(balise)
-{
-    let baliseASupprimer = document.getElementById(`erreurMessage${balise}`)
-    if(baliseASupprimer)
-    {
-        baliseASupprimer.remove()
-    }
+function viderBaliseErreur(balise) {
+  const baliseASupprimer = document.getElementById(`erreurMessage${balise}`);
+  if (baliseASupprimer) {
+    baliseASupprimer.remove();
+  }
 }
